70d8ee0879a9fe236e5fda36683b60745501149a
sovetbek <akmatkulovs991@gmail.com>
2022-01-05 18:07:22 +0300

    Update app

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sqlite3'
 
 db = SQLite3::Database.new 'database.sqlite'
-db.execute "INSERT INTO Cars (Name, Price) VALUES ('Jaguar', 7777)"
+db.execute "INSERT INTO Cars (Name, Price) VALUES ('Mazda', 8600)"
 db.close
