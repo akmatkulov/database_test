6805357e4d0f2bef6ef9f01cd170414672579a50
sovetbek <akmatkulovs991@gmail.com>
2022-01-05 18:24:17 +0300

    Update app

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require 'sqlite3'
 
 db = SQLite3::Database.new 'database.sqlite'
-db.execute "INSERT INTO Cars (Name, Price) VALUES ('Reno', 5600)"
+db.execute "SELECT * FROM Cars" do |car|
+  puts car
+  puts "====="
+end
 db.close
